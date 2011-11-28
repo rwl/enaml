52a4f04b7dbb7017052067592a274d78e4c7660c
Chris Colbert <sccolbert@gmail.com>
2011-11-28 15:41:28 -0500

    stub out selection model

---
+++ enaml/item_models/selection_model.py
@@ -0,0 +1,13 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2011, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+from traits.api import HasTraits
+
+
+class SelectionModel(HasTraits):
+    """ Stub class for implementing a selection model.
+
+    """
+    pass
+
